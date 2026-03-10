c4902734bcd0042b5df8ec9c6982ac44f88eae18
Muhammad rizkya nasti firmansyah <rizkyarancid@gmail.com>
2026-03-10 22:53:16 +0800

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
+// Halaman utama langsung menampilkan Portfolio
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Tugas Grid Ungu 1
 Route::get('/grid', function () {
     return view('tugas_grid');
 });
 
-Route::get('/grid/Mlaravel', function () {
+// Tugas Grid Ungu 2
+Route::get('/grid/mlaravel', function () {
     return view('tugas_grid_Mlaravel');
 });
 
+// Portofolio (bisa diakses via / atau /portfolio)
 Route::get('/portfolio', function () {
     return view('portfolio');
 });
